c6efa7e67af9f1160400406d462016b941b6c4e0
Jennifer Buehler <jenniferb@cse.unsw.edu.au>
2016-06-08 14:28:26 +0200

    Rebase from 'upstream'

--- urdf2graspit/include/urdf2graspit/DHParam.h
+++ urdf2graspit/include/urdf2graspit/DHParam.h
@@ -21,7 +21,7 @@
 // Copyright Jennifer Buehler
 
 #include <urdf/model.h>
-
+#include <urdf_traverser/Types.h>
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 
@@ -30,16 +30,22 @@ namespace urdf2graspit
 
 /**
  * \brief Class providing functions for conversion from urdf to a model described by denavit-hartenberg parameters
+ * 
+ * DH Parameters given in this class transform from a reference frame (i) to (i+1).
+ * The URDF joint is located at frame (i).
+ *
  * \author Jennifer Buehler
  * \date last edited October 2015
  */
 class DHParam
 {
 public:
-    typedef Eigen::Transform<double, 3, Eigen::Affine> EigenTransform;
+    typedef urdf_traverser::EigenTransform EigenTransform;
+    typedef urdf_traverser::JointConstPtr JointConstPtr;
+    typedef urdf_traverser::LinkConstPtr LinkConstPtr;
 
     DHParam():
-        joint(boost::shared_ptr<urdf::Joint>()),
+        joint(JointConstPtr()),
         dof_index(-1),
         d(0),
         r(0),
@@ -47,26 +53,28 @@ public:
         alpha(0) { }
     DHParam(const DHParam& p):
         joint(p.joint),
+        childLink(p.childLink),
         dof_index(p.dof_index),
         d(p.d),
         r(p.r),
         theta(p.theta),
         alpha(p.alpha) { }
 
-
-    boost::shared_ptr<urdf::Joint> joint;
+    // URDF joint which is located at reference frame (i).
+    // The DH parameters transform to frame (i+1).
+    JointConstPtr joint;
+    LinkConstPtr childLink;
     int dof_index;   // < index of this dh-parameter in the list of DOF specifications of graspit
     double d;  // < distance along previous z
     double r;  // < orthogonal distance from previous z axis to current z
     double theta;  // < rotation of previous x around previous z to current x
     double alpha;  // < rotation of previous z to current z
 
-
-
     DHParam& operator=(const DHParam& p)
     {
         if (&p == this) return *this;
         joint = p.joint;
+        childLink = p.childLink;
         dof_index = p.dof_index;
         d = p.d;
         r = p.r;
@@ -79,14 +87,15 @@ public:
     friend std::ostream& operator<<(std::ostream& o, const DHParam& p)
     {
         o << p.joint->name << ": d=" << p.d << ", r=" << p.r << ", theta=" << p.theta
-          << ", alpha=" << p.alpha << ", didx=" << p.dof_index;
+          << ", alpha=" << p.alpha << ", dof_idx=" << p.dof_index;
         return o;
     }
 
     /**
      * computes denavit hartenberg parameters out of a few world coordinates:
-     * - frame (i-1): zi_1, xi_i pi_1 are z axis (rotation axis), x axis (obtained from previous calls to this function for joints earlier in the chain, or any for root joint)
-     * and pi_1 is the world position of the frame.
+     * - frame (i-1): zi_1, xi_i pi_1 are z axis (rotation axis), x axis (obtained
+     *      from previous calls to this function for joints earlier in the chain, or any for root joint)
+     *      and pi_1 is the world position of the frame.
      * - frame i: zi and pi is z-axis and position of frame i.
      * \param xi the new x-axis of frame i will be returned in this paramter.
      */
@@ -104,12 +113,22 @@ public:
     static EigenTransform getTransform(const DHParam& p);
 
 
-
+    /**
+     * For each child link defined in the DH parameters \e dh the transform
+     * the transform from DH to URDF space is returned in \e transforms. The transform is given in the
+     * local joint space.
+     * \param transforms for each link, the transform from DH to URDF space
+     * \retval false if there is a consistency error
+     * \param dh2urdf if true, return transforms from DH to URDF. Otherwise, return it the other way round.
+     */
+    static bool getTransforms(const std::vector<DHParam>& dh, const bool dh2urdf, std::map<std::string,EigenTransform>& transforms);
 
 private:
     /**
-     * Calcuates DH-Paramters r and alpha between to frames i-1 and i. r is the distance between them along the common normal. alpha is the angle
-     * between both z-axises. At the same time, returns the origin of the common normal between frames i-1 and i, and its direction (normalized, but can be re-scaled
+     * Calcuates DH-Paramters r and alpha between to frames i-1 and i. r is
+     * the distance between them along the common normal. alpha is the angle
+     * between both z-axises. At the same time, returns the origin of the common normal
+     * between frames i-1 and i, and its direction (normalized, but can be re-scaled
      * to length between frames by multiplying with r).
      * \param pi_1 and p_i position in world coordinates of frames i-1 and i
      * \param zi_1 and z_i z-axises of both frames i-1 and i
@@ -130,12 +149,20 @@ private:
 
     /**
      * gets the common normal between zi_1 (through pi_1) and zi (through pi).
-     * \param nOriginOnZi_1 returns the point on zi_1 which is shortest from zi (this is intersection point if they intersect, or pi_1 if they are parallel)
+     * \param nOriginOnZi_1 returns the point on zi_1 which is shortest from zi
+     *      (this is intersection point if they intersect, or pi_1 if they are parallel)
      * \param shortestDistance the length of the common normal, or also the shortest distance between the lines.
      */
     static bool getCommonNormal(const Eigen::Vector3d& zi_1, const Eigen::Vector3d& zi, const Eigen::Vector3d& pi_1,
                                 const Eigen::Vector3d& pi, Eigen::Vector3d& commonNormal,
                                 Eigen::Vector3d& nOriginOnZi_1, double& shortestDistance, bool& parallel);
+
+
+    static EigenTransform getTransform(const urdf::Pose& p);
+
+    // Get joint transform to parent
+    static EigenTransform getTransform(const DHParam::JointConstPtr& joint);
+
 };
 
 }  //  namespace urdf2graspit
